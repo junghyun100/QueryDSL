679e3dba755aba4ac6d1c6e41053c1fb87603761
Junghyun100 <qorwjdgus8951@gmail.com>
2022-06-13 17:22:25 +0900

    QueryDsl 셋팅 및 검증

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -3,6 +3,8 @@ package study.querydsl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class QuerydslApplication {
 
--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.QQuerydslTest;
+import study.querydsl.entity.QuerydslTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager entityManager;
 
 	@Test
 	void contextLoads() {
+		QuerydslTest test = new QuerydslTest();
+		entityManager.persist(test);
+		JPAQueryFactory query = new JPAQueryFactory(entityManager);
+
+		QQuerydslTest qQuerydslTest = new QQuerydslTest("h");
+		QuerydslTest result = query
+				.selectFrom(qQuerydslTest)
+				.fetchOne();
+		Assertions.assertThat(result).isEqualTo(test);
 	}
 
 }
