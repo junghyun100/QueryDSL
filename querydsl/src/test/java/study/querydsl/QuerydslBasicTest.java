a03abe92bae5f8defe0c65824f6b2c47977b486a
Junghyun100 <qorwjdgus8951@gmail.com>
2022-06-18 20:53:46 +0900

    fetch를 통한 조회와 fetchResults를 대신할 수 있는 방안

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,4 +1,5 @@
 package study.querydsl;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -90,4 +91,43 @@ public class QuerydslBasicTest {
         member.username.startsWith("member") //like ‘member%’ 검색
      */
 
+
+    @Test
+    public void resultFetchTest() {
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch();
+//
+//        /*
+//        * com.querydsl.core.NonUniqueResultException:
+//        * javax.persistence.NonUniqueResultException: query did not return a unique result: 4
+//        * 1개에 대한 조회를 실행했는데 4개가 있어서 나타나는 에러
+//        *  */
+//        Member fetchOne = queryFactory
+//                .selectFrom(member)
+//                .fetchOne();
+//
+//        Member fetchFirst = queryFactory
+//                .selectFrom(member)
+//                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        /**
+         * queryDsl의 fetchResult의 경우 count를 하기위해선 count용 쿼리를 만들어서 실행해야 하는데,
+         * 카운트를 하려는 select 쿼리를 기반으로 count 쿼리를 만들어 실행한다.
+         * SELECT COUNT(*) FROM (<original query>).
+         *
+         * 그런데 이게 단순한 쿼리에서는 잘 동작하는데, 복잡한 쿼리(다중그룹 쿼리)에서는 잘 작동하지 않는다고 한다.
+         * groupby having 절을 사용하는 등의 복잡한 쿼리 문에서 예외가 떠버리는 것으로 보이고
+         *
+         * 더불어 대부분의 dialect에서는 count쿼리가 유효하지만 JPQL에서는 아니란다.
+         * 그렇기 때문에 fetchResult보다는 fetch를 사용하고 따로 fetchCount를 사용하거나 list의 count를 사용하는것이 좋을 것 같다!
+         */
+    }
 }
