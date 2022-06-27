1d925a2aa887cc400e42c76997647aa96aa563b5
Junghyun100 <qorwjdgus8951@gmail.com>
2022-06-27 21:26:15 +0900

    프로젝션과 결과 반환 - 기본

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -484,4 +484,36 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+    /*
+    프로젝션이란? select를 통해 대상 지정하고 데이터를 가져오는 것을 말한다.
+     */
+    @Test
+    public void simpleProjection() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * 프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+     * 프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+     * */
+    @Test
+    public void tupleProjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username=" + username);
+            System.out.println("age=" + age);
+        }
+    }
 }
